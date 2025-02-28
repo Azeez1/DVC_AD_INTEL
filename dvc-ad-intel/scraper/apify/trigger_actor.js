8fa6b6eb28dbbe7bcd2f8bde933f3c65d96f5903
Azeez1 <sabaazeez12@gmail.com>
2025-02-28 18:07:46 +0000

    update trigger_actor

--- dvc-ad-intel/scraper/apify/trigger_actor.js
+++ dvc-ad-intel/scraper/apify/trigger_actor.js
@@ -1,6 +1,6 @@
 // File: trigger_actor.js
 // Description: This script triggers the actor "curious_coder/facebook-ads-library-scraper"
-// using the standard run method and retrieves dataset items.
+// with the correct input format including the required "urls" field
 
 const { ApifyClient } = require('apify-client');
 
@@ -9,16 +9,24 @@ const client = new ApifyClient({
     token: 'apify_api_Cs25DCKxbaabAfdKjGDJkMqYaprUST48hBm8',
 });
 
-// Prepare the actor input.
+// Prepare the actor input with the required "urls" field
+// Format the input according to the actor's expected format
 const input = {
-    searchQuery: "shapewear",
-    count: 20
+    urls: [
+        `https://www.facebook.com/ads/library/?active_status=all&ad_type=all&country=US&q=${encodeURIComponent("shapewear")}&sort_data[direction]=desc&sort_data[mode]=relevancy_monthly_grouped&search_type=keyword_unordered`
+    ],
+    maxPagesPerSearch: 5,   // Limit the number of pages to scrape
+    maxAdsPerPage: 20,      // Limit the number of ads per page
+    extendOutputFunction: "",
+    language: "en-US"
 };
 
 (async () => {
     try {
         // Run the actor and wait for it to finish
-        console.log('Starting actor run...');
+        console.log('Starting actor run with the following input:');
+        console.log(JSON.stringify(input, null, 2));
+
         const run = await client.actor("curious_coder/facebook-ads-library-scraper").call(input);
 
         console.log('Actor run finished successfully.');
